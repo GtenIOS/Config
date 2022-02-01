4d75b1319359c0fac93b556e10458b79575aa8e0
Jiten Devlani <jdevlani@yahoo.in>
2022-02-01 11:40:57 +0530

    Updated emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,7 +9,15 @@
 (unless (package-installed-p 'evil)
   (package-install 'evil))
 
-;; evil leader (must be initialised before evil modee)
+;; Download multiple-cursors
+;; (unless (package-installed-p 'multiple-cursors)
+;;   (package-install 'multiple-cursors))
+
+;; evil commentary
+(require 'evil-commentary)
+
+;; evil leader (must be initialised before evil mode)
+(require 'evil-leader)
 (global-evil-leader-mode)
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key "e" 'find-file)
@@ -23,7 +31,66 @@
 (evil-mode 1)
 (setq evil-want-fine-undo t)
 (advice-add 'undo-auto--last-boundary-amalgamating-number
-            :override #'ignore)
+             :override #'ignore)
+(setq evil-undo-system 'undo-tree)
+
+;; Enable multiple cursors
+;; (require 'multiple-cursors)
+;; (multiple-cursors-mode 1)
+;; (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+
+;; evil-mc (Evil multiple cursors)
+(when (fboundp 'add-to-load-path)
+  (add-to-load-path (file-name-directory (buffer-file-name))))
+
+;; Download evil-mc
+(unless (package-installed-p 'evil-mc)
+  (package-install 'evil-mc))
+
+(require 'evil-mc)
+
+;; Enable evil-mc
+(global-evil-mc-mode 1)
+
+(evil-define-local-var evil-mc-custom-paused nil
+  "Paused functionality when there are multiple cursors active.")
+
+(defun evil-mc-pause-smartchr-for-mode (mode)
+  "Temporarily disables the smartchr keys for MODE."
+  (let ((m-mode (if (atom mode) mode (car mode)))
+        (s-mode (if (atom mode) mode (cdr mode))))
+    (let ((init (intern (concat "smartchr/init-" (symbol-name s-mode))))
+          (undo (intern (concat "smartchr/undo-" (symbol-name s-mode)))))
+      (when (eq major-mode m-mode)
+        (funcall undo)
+        (push `(lambda () (,init)) evil-mc-custom-paused)))))
+
+(defun evil-mc-before-cursors-setup-hook ()
+  "Hook to run before any cursor is created.
+Can be used to temporarily disable any functionality that doesn't
+play well with `evil-mc'."
+  (mapc 'evil-mc-pause-smartchr-for-mode
+        '(web-mode js2-mode java-mode (enh-ruby-mode . ruby-mode) css-mode))
+  (when (boundp 'whitespace-cleanup-disabled)
+    (setq whitespace-cleanup-disabled t)
+    (push (lambda () (setq whitespace-cleanup-disabled nil)) evil-mc-custom-paused)))
+
+(defun evil-mc-after-cursors-teardown-hook ()
+  "Hook to run after all cursors are deleted."
+  (dolist (fn evil-mc-custom-paused) (funcall fn))
+  (setq evil-mc-custom-paused nil))
+
+(add-hook 'evil-mc-before-cursors-created 'evil-mc-before-cursors-setup-hook)
+(add-hook 'evil-mc-after-cursors-deleted 'evil-mc-after-cursors-teardown-hook)
+
+(defvar evil-mc-mode-line-prefix "ⓜ"
+  "Override of the default mode line string for `evil-mc-mode'.")
+
+; (evil-leader/set-key "k" 'evil-mc-make-cursor-move-prev-line)
+; (evil-leader/set-key "j" 'evil-mc-make-cursor-move-next-line)
+(global-set-key (kbd "C-S-<up>") 'evil-mc-make-cursor-move-prev-line)
+(global-set-key (kbd "C-S-<down>") 'evil-mc-make-cursor-move-next-line)
+(global-set-key (kbd "C-S-c") 'evil-mc-undo-all-cursors)
 
 ;; use-package
 (unless (package-installed-p 'use-package)
@@ -44,11 +111,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("e1b843dd5b1c7b565c9e07e0ecb2fe02440abd139206bd238a2fc0a068b48f84" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "30b14930bec4ada72f48417158155bc38dd35451e0f75b900febd355cda75c3e" default))
+   '("3d2e532b010eeb2f5e09c79f0b3a277bfc268ca91a59cdda7ffd056b868a03bc" "e1b843dd5b1c7b565c9e07e0ecb2fe02440abd139206bd238a2fc0a068b48f84" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "30b14930bec4ada72f48417158155bc38dd35451e0f75b900febd355cda75c3e" default))
  '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode t)
+ '(menu-bar-mode nil)
  '(package-selected-packages
-   '(evil-leader clues-theme evil-commentary all-the-icons-dired dired-sidebar all-the-icons git-gutter affe fzf flycheck yasnippet company lsp-ui lsp-mode rustic solarized-theme gruvbox-theme evil))
+   '(undo-tree evil-mc evil-surround evil-leader clues-theme evil-commentary all-the-icons-dired dired-sidebar all-the-icons git-gutter affe fzf flycheck yasnippet company lsp-ui lsp-mode rustic solarized-theme gruvbox-theme evil))
+ '(scroll-bar-mode nil)
  '(toggle-scroll-bar nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -56,21 +125,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 160 :family "Victor Mono"))))
- '(font-lock-comment-face ((t (:inherit default :slant italic :family "Victor Mono"))))
- '(font-lock-constant-face ((t (:slant italic :weight bold :family "Victor Mono"))))
- '(font-lock-function-name-face ((t (:slant italic :weight bold :family "Victor Mono"))))
- '(font-lock-keyword-face ((t (:slant italic :weight bold :family "Victor Mono"))))
- '(font-lock-preprocessor-face ((t (:weight bold :family "Victor Mono"))))
- '(font-lock-string-face ((t (:slant italic :family "Victor Mono"))))
- '(hl-todo ((t (:slant italic :weight bold :family "Victor Mono"))))
- '(info-colors-lisp-code-block ((t (:inherit font-lock-variable-name-face))))
- '(markdown-code-face ((t (:inherit font-lock-constant-face)))))
+ '(default ((t (:height 140 :family "Iosevka" :weight normal :width normal :spacing 100)))))
+
+;; Gruber-darker 
+(require 'gruber-darker-theme)
+(load-theme 'gruber-darker)
 
 ;; Solarized theme
-;;(load-theme 'solarized-light t)
+;; (load-theme 'solarized-light t)
 ;; (load-theme 'gruvbox-dark-soft t)
-(load-theme 'classic t)
+;; (load-theme 'classic t)
+
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
 
 ;; Rust
 (use-package rustic
@@ -172,6 +241,18 @@
   (add-hook 'text-mode-hook 'yas-minor-mode))
 
 (use-package flycheck :ensure)
+
+;; C
+(require 'ccls)
+(use-package ccls
+  :hook ((c-mode c++-mode objc-mode cuda-mode) .
+         (lambda () (require 'ccls) (lsp))))
+(setq ccls-executable "/usr/local/bin/ccls")
+
+;; indent
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
 
 ;; fzf
 (use-package fzf :ensure)
