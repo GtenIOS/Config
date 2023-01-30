cd7ff70af07499f0164802a3c388c12ad4d78f54
GtenIOS <jdevlani@yahoo.in>
2023-01-30 13:08:28 +0530

    Tabs configuration for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,7 +20,7 @@ vim.g.maplocalleader = ' '
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 require('lazy').setup({
-   { -- LSP Configuration & Plugins
+  { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
@@ -36,7 +36,7 @@ require('lazy').setup({
   },
 
   {
-  "nvim-neo-tree/neo-tree.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -45,42 +45,42 @@ require('lazy').setup({
     }
   },
 
-   { -- Autocompletion
+  { -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
 
-   { -- Highlight, edit, and navigate code
+  { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
   },
 
-   { -- Additional text objects via treesitter
+  { -- Additional text objects via treesitter
     'nvim-treesitter/nvim-treesitter-textobjects',
     after = 'nvim-treesitter',
   },
 
   -- Git related plugins
-   'tpope/vim-fugitive',
-   'tpope/vim-rhubarb',
-   'lewis6991/gitsigns.nvim',
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+  'lewis6991/gitsigns.nvim',
 
-   'navarasu/onedark.nvim', -- Theme inspired by Atom
-   'nvim-lualine/lualine.nvim', -- Fancier statusline
-   'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
-   'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
-   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'navarasu/onedark.nvim', -- Theme inspired by Atom
+  'nvim-lualine/lualine.nvim', -- Fancier statusline
+  'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+  'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
-   { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   -- Fuzzy Finder Algorithm which dependencies local dependencies to be built. Only load if `make` is available
-   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
 
   -- ATS
-   { 'vmchale/ats-vim' },
+  { 'vmchale/ats-vim' },
 })
 
 -- Custom lua codes
@@ -94,6 +94,11 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.o.rnu = true
+
+-- Tab/Spacing config
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
