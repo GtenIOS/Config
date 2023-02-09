780a64a21fd645125e5f2becf70273a0c88fdb75
GtenIOS <jdevlani@yahoo.in>
2023-02-09 16:30:18 +0530

    Telescope update, display all files by default

--- nvim/lua/plugin_config/telescope.lua
+++ nvim/lua/plugin_config/telescope.lua
@@ -9,6 +9,13 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      hidden = true,
+      follow = true,
+      no_ignore = true,
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
@@ -25,7 +32,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = '[G]Git [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
