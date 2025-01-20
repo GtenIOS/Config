3e55b547a417829c1301d64168176f4c6cdb8355
GtenIOS <jdevlani@yahoo.in>
2025-01-20 21:56:46 +0530

    latest config from NixOS

--- nvim/lua/plugin_config/lsp.lua
+++ nvim/lua/plugin_config/lsp.lua
@@ -55,7 +55,7 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  clangd = {},
+  clangd = { filetypes = {"c"}},
   -- gopls = {},
   -- pyright = {},
   rust_analyzer = {
@@ -64,11 +64,21 @@ local servers = {
     }
   },
   zls = {},
-  intelephense = {
-    format = {
-      enable = true,
+  ols = {},
+  ocamllsp = {},
+  hls = { cmd = "haskell-language-server-wrapper", },
+  serve_d = {
+    command = "serve-d",
+    d = { 
+      dcdClientPath = "../DCD/bin/dcd-client",
+      dcdServerPath = "../DCD/bin/dcd-server",
     }
   },
+  -- intelephense = {
+  --   format = {
+  --     enable = true,
+  --   }
+  -- },
   -- tsserver = {},
 }
 
@@ -80,24 +90,32 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Setup mason so it can manage external tooling
-require('mason').setup()
-
+-- require('mason').setup()
+--
 -- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
+-- local mason_lspconfig = require 'mason-lspconfig'
+--
+-- mason_lspconfig.setup {
+--   ensure_installed = vim.tbl_keys(servers),
+-- }
+--
+-- mason_lspconfig.setup_handlers {
+--   function(server_name)
+--     require('lspconfig')[server_name].setup {
+--       capabilities = capabilities,
+--       on_attach = on_attach,
+--       settings = servers[server_name],
+--     }
+--   end,
+-- }
 
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-}
+for server_name,server_settings in pairs(servers) do 
+  require('lspconfig')[server_name].setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = server_settings,
+  }
+end
 
 -- Turn on lsp status information
 require('fidget').setup()
@@ -111,10 +129,60 @@ luasnip.config.set_config({
   delete_check_events = 'InsertLeave'
 })
 
+local cmp_kinds = {
+  Text = ' ',
+  Method = ' ',
+  Function = ' ',
+  Constructor = ' ',
+  Field = ' ',
+  Variable = ' ',
+  Class = ' ',
+  Interface = ' ',
+  Module = ' ',
+  Property = ' ',
+  Unit = ' ',
+  Value = ' ',
+  Enum = ' ',
+  Keyword = ' ',
+  Snippet = ' ',
+  Color = ' ',
+  File = ' ',
+  Reference = ' ',
+  Folder = ' ',
+  EnumMember = ' ',
+  Constant = ' ',
+  Struct = ' ',
+  Event = ' ',
+  Operator = ' ',
+  TypeParameter = ' ',
+}
+
 cmp.setup {
+  preselect = cmp.PreselectMode.None,
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+  },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered {
+      border = 'single',
+      winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
+    },
+    documentation = cmp.config.window.bordered {
+      border = 'single',
+      winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
+    },
+  },
+  formatting = {
+    fields = { 'kind', 'abbr' },
+    format = function(_, vim_item)
+      vim_item.kind = cmp_kinds[vim_item.kind] or ''
+      vim_item.menu = ''
+      return vim_item
     end,
   },
   mapping = cmp.mapping.preset.insert {
@@ -147,6 +215,9 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = "calc" },
+    { name = "path" },
+    { name = "rg", keyword_length = 3 },
   },
 }
 
